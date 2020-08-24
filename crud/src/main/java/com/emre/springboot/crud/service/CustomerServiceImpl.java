4decdfe1e4459be6a350d56acbbaa50c7d590fe9
sahinemre1985 <sahinemre1985@gmail.com>
2020-08-24 13:21:50 +0300

    Third Commit - Spring Data Jpa
    Instead of DAO we used Jpa repository. Only an interface which extends
    JpaRepository and parameters of entity and pk. All of dao interfaces and
    their implementations can be deleted.
    
    In service layer, code refactored according to JPA Repository

--- crud/src/main/java/com/emre/springboot/crud/service/CustomerServiceImpl.java
+++ crud/src/main/java/com/emre/springboot/crud/service/CustomerServiceImpl.java
@@ -1,50 +1,61 @@
 package com.emre.springboot.crud.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.emre.springboot.crud.dao.CustomerDAO;
+import com.emre.springboot.crud.dao.CustomerRepository;
 import com.emre.springboot.crud.entity.Customer;
 
 @Service
 public class CustomerServiceImpl implements CustomerService {
 
 	
-	private CustomerDAO customerDAO;
+	private CustomerRepository customerRepository;
 	
 	@Autowired
-	public CustomerServiceImpl(@Qualifier("customerDAOJpaImpl") CustomerDAO theCustomerDAO) {
-		customerDAO = theCustomerDAO;
+	public CustomerServiceImpl( CustomerRepository theCustomerRepository) {
+		customerRepository = theCustomerRepository;
 	}
 
 	@Override
 	@Transactional
 	public List<Customer> findAll() {
-		return customerDAO.findAll();
+		return customerRepository.findAll();
 	}
 
 	@Override
 	@Transactional
 	public Customer findById(int theId) {
-		return customerDAO.findById(theId);
+		Optional<Customer> result = customerRepository.findById(theId);
+		
+		Customer theCustomer=null;
+		
+		if(result.isPresent()) {
+			theCustomer=result.get();
+		}
+		else {
+			throw new RuntimeException("did not find customer id -" + theId);
+		}
+		
+		return theCustomer;
 	}
 
 	@Override
 	@Transactional
 	public void save(Customer theCustomer) {
 		
-		customerDAO.save(theCustomer);
+		customerRepository.save(theCustomer);
 	}
 
 	@Override
 	@Transactional
 	public void deleteById(int theId) {
 		
-		customerDAO.deleteById(theId);
+		customerRepository.deleteById(theId);
 	}
 	
 	
